3072c82c83437b4d2281e1757211980fab89118e
STas <108541890+stas159153@users.noreply.github.com>
2023-02-10 21:03:19 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let a = Number (prompt ( "Enter namber" ));
+let b = Number (prompt ( "Enter namber" ));
+if ( a + b > 10 ) {
+ alert ( "Ok"  )
+
+ 
+}
+else{
+    alert( "V_Ananas_idite" );
+}
+let c = prompt ( " Enter Your Name " );
+if ( c ==  "Andrew" ) {
+    alert ( " Pruvit " + " " + c );
+}
+else{
+    alert( "Xto tu" );
+   
+}
